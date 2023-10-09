2d1f46076ecca619e9dc98ef8eb99d7935389f68
naufal11705 <ahmadaji17@smk.belajar.id>
2023-10-09 14:19:54 +0700

    menambah rata rata

--- JobSheet7/ForKelipatan04.java
+++ JobSheet7/ForKelipatan04.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ForKelipatan04 {
 
     public static void main(String[] args) {
-        int kelipatan, jumlah=0, counter=0;
+        int kelipatan, jumlah=0, counter=0, totalKelipatan=0;
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Masukkan bilangan kelipatan (1-9): ");
@@ -13,10 +13,12 @@ public class ForKelipatan04 {
             if (i % kelipatan == 0) {
                 jumlah += i;
                 counter++;
+                totalKelipatan += i;
             }
         }
 
         System.out.println("Banyaknya bilangan "+ kelipatan +" dari 1 sampai 50 adalah "+ counter);
         System.out.println("Total Bilangan kelipatan "+ kelipatan +" dari 1 sampai 50 adalah "+ jumlah);
+        System.out.println("Rata-rata dari seluruh bilangan kelipatan "+ kelipatan +" dari 1 sampai 50 adalah "+ (totalKelipatan / kelipatan));
     }
 }

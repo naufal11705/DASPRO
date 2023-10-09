3a0f7757eecb6399a0362bba045693336bf244b3
naufal11705 <ahmadaji17@smk.belajar.id>
2023-10-09 11:18:32 +0700

    create WhileKelipatan04.java

---
+++ JobSheet7/WhileKelipatan04.java
@@ -0,0 +1,24 @@
+package JobSheet7;
+
+import java.util.Scanner;
+
+public class WhileKelipatan04 {
+    public static void main(String[] args) {
+        int kelipatan, jumlah=0, counter=0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        int i = 0;
+        while (i <= 50){
+            i++;
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        System.out.println("Banyaknya bilangan "+ kelipatan +" dari 1 sampai 50 adalah "+ counter);
+        System.out.println("Total Bilangan kelipatan "+ kelipatan +" dari 1 sampai 50 adalah "+ jumlah);
+    }
+}

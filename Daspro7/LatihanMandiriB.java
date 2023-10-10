bf9eab35d10fb410c7898d6b64ce7a44d678b725
naufal11705 <ahmadaji17@smk.belajar.id>
2023-10-10 20:51:04 +0700

    create latihan mandiri

---
+++ Daspro7/LatihanMandiriB.java
@@ -0,0 +1,13 @@
+package Daspro7;
+
+public class LatihanMandiriB {
+    public static void main(String[] args) {
+        int hasil = 0;
+
+        for (int i = 25; i >= 1; i--) {
+            hasil += i;
+        }
+
+        System.out.println("Hasil penjumlahan deret bilangan dari 25 sampai dengan 1 adalah: " + hasil);
+    }
+}
---
+++ Daspro7/LatihanMandiriC.java
@@ -0,0 +1,13 @@
+package Daspro7;
+
+public class LatihanMandiriC {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            if (i % 3 != 0) {
+                System.out.print(i + " ");
+            }
+            i++;
+        }
+    }    
+}

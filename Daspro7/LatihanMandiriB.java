e0c3290841c997da4e91c30f42fa96cacf50da2f
naufal11705 <ahmadaji17@smk.belajar.id>
2023-10-10 21:34:02 +0700

    fix (again)

--- Daspro7/LatihanMandiriB.java
+++ Daspro7/LatihanMandiriB.java
@@ -2,11 +2,11 @@ package Daspro7;
 
 public class LatihanMandiriB {
     public static void main(String[] args) {
-        int i = 25, hasil = 0;
+        int i = 1, hasil = 0;
 
-        while (i >= 1) {
+        while (i <= 25) {
             hasil += i;
-            i--; 
+            i++; 
         }
 
         System.out.println("Hasil penjumlahan deret bilangan dari 25 sampai dengan 1 adalah: " + hasil);

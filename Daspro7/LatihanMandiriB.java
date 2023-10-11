0600b73ee3e22684ee3a059774a14f7f505969ed
naufal11705 <ahmadaji17@smk.belajar.id>
2023-10-11 11:24:00 +0700

    add deskripsi

--- Daspro7/LatihanMandiriB.java
+++ Daspro7/LatihanMandiriB.java
@@ -2,8 +2,9 @@ package Daspro7;
 
 public class LatihanMandiriB {
     public static void main(String[] args) {
+        //inisialisasi
         int i = 1, hasil = 0;
-
+        //melakukan perulangan hingga i <= 25
         while (i <= 25) {
             hasil += i;
             i++; 
--- Daspro7/LatihanMandiriC.java
+++ Daspro7/LatihanMandiriC.java
@@ -2,8 +2,11 @@ package Daspro7;
 
 public class LatihanMandiriC {
     public static void main(String[] args) {
+        //inisialisasi
         int i = 1;
+        //kondisi perulangan
         while (i <= 50) {
+            //Menampilkan deret bilangan 1 sampai 50, kecuali bilangan kelipatan 3
             if (i % 3 != 0) {
                 System.out.print(i + " ");
             }

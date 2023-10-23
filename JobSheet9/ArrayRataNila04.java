5bafd536483197117a9380a8922e4e2d97af4eb7
naufal11705 <ahmadnaufal11705@gmail.com>
2023-10-23 10:45:33 +0700

    add rata2 lulus & tdklulus

--- JobSheet9/ArrayRataNila04.java
+++ JobSheet9/ArrayRataNila04.java
@@ -5,28 +5,36 @@ public class ArrayRataNila04 {
     public static void main(String[] args) {
         Scanner sc04 = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
-
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        double rata2Lulus = 0;
+        double rata2TdkLulus = 0;
+        int lulus = 0;
+        int tdkLulus = 0;
+        //Memasukkan jumlah mahasiswa
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int length = sc04.nextInt();
+        //Kapasitas array disesuaikan sesuai jumlah mahasiswa
+        int[] nilaiMhs = new int[length];
+        //Memasukkan nilai mahasiswa
+        for (int i = 0 ; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc04.nextInt();
         }
-
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        
+        for (int i = 0 ; i < nilaiMhs.length; i++) {
+            //Menghitung jumlah dan nilai dari mahasiswa dengan nilai
+            //lebih dari 70
             if (nilaiMhs[i] > 70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus!");
+                rata2Lulus += nilaiMhs[i];
+                lulus++;
             } else {
-                System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+            //Menghitung jumlah dan nilai dari mahasiswa dengan nilai
+            //kurang dari 70
+                rata2TdkLulus += nilaiMhs[i];
+                tdkLulus++;
             }
         }
+        //Printout rata rata nilai
+        System.out.println("Rata-rata nilai lulus= "+(rata2Lulus/lulus));
+        System.out.println("Rata-rata nilai tidak lulus= "+(rata2TdkLulus/tdkLulus));
     }
 }

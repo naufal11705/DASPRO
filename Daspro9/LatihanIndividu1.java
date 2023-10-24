09dfe7c93e0adafae71635e18d7d7cd308543065
naufal11705 <ahmadnaufal11705@gmail.com>
2023-10-24 20:47:40 +0700

    create LatihanIndividu

---
+++ Daspro9/LatihanIndividu1.java
@@ -0,0 +1,15 @@
+package Daspro9;
+
+import java.util.Scanner;
+
+public class LatihanIndividu1 {
+    public static void main(String[] args) {
+        Scanner sc04 = new Scanner(System.in);
+
+        int[] nilai = new int[60];
+
+        for (int i = 0; i < nilai.length; i++) {
+            nilai[i] = i;
+        }
+    }
+}

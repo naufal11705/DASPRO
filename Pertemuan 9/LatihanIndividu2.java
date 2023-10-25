a04e9e091fc4d3b4466d81c7b21f3567a103b1d2
naufal11705 <ahmadnaufal11705@gmail.com>
2023-10-25 09:45:18 +0700

    fix

--- Pertemuan 9/LatihanIndividu2.java
+++ Pertemuan 9/LatihanIndividu2.java
@@ -11,22 +11,9 @@ public class LatihanIndividu2 {
             nilai[i] = sc04.nextInt();
         }
 
-        int temp = 0;
-
-        for (int i=0; i < nilai.length; i++) {
-            for (int j=1; j < nilai.length-i; j++) {
-                if (nilai[j-1] < nilai[j]) {
-                    //swap elemen
-                    temp = nilai[j];
-                    nilai[j] = nilai[j-1];
-                    nilai[j-1] = temp;
-                }
-            }
-        }
-
-        System.out.println("Hasil pengurutan:");
-        for (int i=0; i < nilai.length; i++) {
+        System.out.println("Menampilkan Secara Terbalik:");
+        for (int i=9; i > -1; i--) {
             System.out.println(nilai[i]);
-        }        
+        }    
     }
 }

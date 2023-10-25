6d23fce8609ff6a1e92e4d0ef5a91708fdfa7a64
naufal11705 <ahmadnaufal11705@gmail.com>
2023-10-25 10:26:47 +0700

    modifikasi

--- Pertemuan 9/LinearSearch04.java
+++ Pertemuan 9/LinearSearch04.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class LinearSearch04 {
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc04 = new Scanner(System.in);
+
         int hasil = 0;
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        int length = sc04.nextInt();
+
+        int[] arrayInt = new int[length];
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            arrayInt[i] = sc04.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc04.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {

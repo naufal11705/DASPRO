636f504281da1288a88cfcc29a00a0571ae11af6
Ahmad Naufal Waskito Aji <ahmadnaufal11705@gmail.com>
2023-11-27 16:52:52 +0700

    modifikasi

--- Pertemuan 13/Percobaan604.java
+++ Pertemuan 13/Percobaan604.java
@@ -11,10 +11,26 @@ public class Percobaan604 {
         int volume = hitungLuas(a, b)*tinggi;
         return volume;
     }
+
+    static int luasPermukaan1 (int pjg, int lb) {
+        int Lp1 = pjg*lb*2;
+        return Lp1;
+    }
+
+    static int luasPermukaan2 (int pjg, int tinggi) {
+        int Lp2 = pjg*tinggi*2;
+        return Lp2;
+    }
+
+    static int luasPermukaan3 (int lb, int tinggi) {
+        int Lp3 = lb*tinggi*2;
+        return Lp3;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int p,l,t,L,vol;
+        int p,l,t,L,vol,lp;
 
         System.out.println("Masukkan panjang");
         p=input.nextInt();
@@ -28,5 +44,8 @@ public class Percobaan604 {
 
         vol=hitungVolume(t, p, l);
         System.out.println("Volume balok adalah "+vol);
+
+        lp= luasPermukaan1(p, l)+luasPermukaan2(p, t)+luasPermukaan3(l, t);
+        System.out.println("Luas Permukaan adalah "+lp);
     }
 }
--- Pertemuan 13/UcapanTerimaKasih_04.java
+++ Pertemuan 13/UcapanTerimaKasih_04.java
@@ -11,8 +11,9 @@ public class UcapanTerimaKasih_04 {
 
     public static void UcapanTerimaKasih(){
         String nama = PenerimaUcapan();
-        System.out.println("Thank you for being the best teacher in the world. \n"+
+        System.out.println("Thank you "+nama+" for being the best teacher in the world. \n"+
         "You inspired in me a love for learning and made me feel like i could ask you anything.");
+        
     }
     
     public static void UcapanTambahan(String ucapan) {
